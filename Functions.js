743f47a181a7ddeab8779999113fa0dbcd520c40
ViktoriaWasTaken <114031171+ViktoriaWasTaken@users.noreply.github.com>
2022-09-30 14:10:02 -0500

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,30 @@
+function doInputOutput() {
+    document.getElementById("Height")
+    document.getElementById("Width")
+    document.getElementById("length")
+    document.getElementById("Sweep")
+    let A = "Height";
+    let B = "Width";
+    let C = "length";
+
+    function houseVolume(Width, length, height, sweep) {
+        document.getElementById("livingVolume" + "roofVolume")
+        return houseVolume(value)
+    }
+    function livingVolume(width, length, height) {
+        document.getElementById("Width" * "Length" * "Height")
+        return livingVolume(value)
+    }
+    function roofVolume(width, length, sweep) {
+        document.getElementById(triangleArea * "Width")
+        return roofVolume(value)
+    }
+    function triangleArea(A, B, C) {
+        let s = (("A" + "B" + "C") / 2)
+
+    }
+    document.querySelector("#btn").addEventListener("click", () => {
+        alert(doInputOutput(Cost.value)).Cost
+    }
+    )
+}
